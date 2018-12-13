7080d0c33e66aa1cf9fad04b9d10acdeb94614b2
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-12-14 00:53:17 +0800

    初始化

--- application/api/controller/v1/Flow.php
+++ application/api/controller/v1/Flow.php
@@ -52,9 +52,13 @@ class Flow extends BaseController
      * @param $run_id
      * @param $run_process
      * @param $wf_fid
-     * @return \think\response\Json
+     * @param $wf_type
+     * @param string $submit_to_save
+     * @return Json
+     * @throws \app\lib\exception\TokenException
+     * @throws \think\Exception
      */
-    public function checkPass($check_con, $flow_id, $flow_process, $npid, $run_id, $run_process, $wf_fid, $wf_type,$submit_to_save='ok')
+    public function checkPass($check_con, $flow_id, $flow_process, $npid, $run_id, $run_process, $wf_fid, $wf_type, $submit_to_save = 'ok')
     {
         /**
          *
@@ -87,7 +91,7 @@ class Flow extends BaseController
             'submit_to_save' => $submit_to_save,
             'wf_fid' => $wf_fid,
             'wf_singflow' => "",
-            'wf_backflow' => "",
+            'wf_backflow' => $submit_to_save == 'ok' ? "" : 0,
             'wf_title' => 2,
             'wf_type' => $wf_type,
         ];
@@ -111,7 +115,6 @@ class Flow extends BaseController
      * @apiSuccessExample {json}返回样例:
      * {"check":1,"info":{"sing_st":0,"flow_id":2,"run_id":12,"status":{"id":12,"uid":1,"run_id":12,"run_flow":2,"run_flow_process":4,"parent_flow":0,"parent_flow_process":0,"run_child":0,"remark":"","is_receive_type":0,"auto_person":5,"sponsor_text":"局长,普通员工,机服中心,机服主管局长,机服负责人,管理员,车队,部门主管局长,部门主管领导,部门局领导,部门负责人","sponsor_ids":"36,27,33,35,34,26,37,31,30,29,28","is_sponsor":0,"is_singpost":0,"is_back":0,"status":0,"js_time":1544516328,"bl_time":0,"jj_time":0,"is_del":0,"updatetime":0,"dateline":1544516328},"flow_process":4,"run_process":12,"flow_name":"门禁权限","process":{"id":4,"process_name":"申请","process_type":"is_one","process_to":"5","auto_person":5,"auto_sponsor_ids":"","auto_role_ids":"36,27,33,35,34,26,37,31,30,29,28","auto_sponsor_text":"","auto_role_text":"局长,普通员工,机服中心,机服主管局长,机服负责人,管理员,车队,部门主管局长,部门主管领导,部门局领导,部门负责人","range_user_ids":"","range_user_text":"","is_sing":1,"sign_look":0,"is_back":1,"todo":"局长,普通员工,机服中心,机服主管局长,机服负责人,管理员,车队,部门主管局长,部门主管领导,部门局领导,部门负责人"},"nexprocess":{"id":5,"process_name":"部门负责人审核","process_type":"is_step","process_to":"6","auto_person":5,"auto_sponsor_ids":"","auto_role_ids":"28","auto_sponsor_text":"","auto_role_text":"部门负责人","range_user_ids":"","range_user_text":"","is_sing":1,"sign_look":0,"is_back":1,"todo":"部门负责人"},"preprocess":["退回制单人修改"],"singuser":[{"id":1,"username":"朱明良","role":1},{"id":2,"username":"部门负责人","role":28},{"id":3,"username":"机服中心","role":33},{"id":4,"username":"机服中心负责人","role":34}],"log":[{"id":2,"uid":1,"from_id":22,"from_table":"access_control_t","run_id":12,"run_flow":0,"content":"同意","dateline":1544516328,"btn":"Send","art":"","user":"朱明良"}]}}
      * @apiSuccess (返回参数说明) {int} check 是否显示审批按钮 ：1 | 显示；2 | 不显示
-
      * @param $wf_fid
      * @param $wf_type
      * @return Json
