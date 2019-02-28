9c74c27a07eade1df93d0188801135379cf38c91
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-02-28 22:24:11 +0800

    初始化

--- application/api/controller/v1/Sku.php
+++ application/api/controller/v1/Sku.php
@@ -257,6 +257,7 @@ class Sku extends BaseController
         (new SkuStockValidate())->goCheck();
         $params = $this->request->param();
         $params['state'] = CommonEnum::STATE_IS_OK;
+        $params['stock_date'] = date('Y-m-d');
         $params['admin_id'] = \app\api\service\Token::getCurrentUid();
         $params['stock'] = (new SkuService())->getStock($params['sku_id'], $params['type'], $params['count']);
         $res = SkuStockT::create($params);
