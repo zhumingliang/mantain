2f0153e3eda6e093bc80cbed4efe51762383a4de
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-05-06 00:59:59 +0800

    初始化

--- application/api/service/AdminToken.php
+++ application/api/service/AdminToken.php
@@ -65,8 +65,10 @@ class AdminToken extends Token
             }
 
             // if (sha1($this->pwd) != $admin->pwd) {
-            echo $admin->pwd;
             if ($this->pwd != $admin->pwd) {
+                echo $this->pwd;
+                echo '\n';
+                echo $admin->pwd;
                 throw new TokenException([
                     'code' => 401,
                     'msg' => '账号或者密码不正确，请检查！',
