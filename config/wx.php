05eaa8d311985d44932d43bdf1d945952fd7fba5
zhumingliang <353575156@qq.com>
2019-09-19 09:35:48 +0800

    初始化

--- config/wx.php
+++ config/wx.php
@@ -6,12 +6,31 @@
  * Time: 4:24 PM
  */
 
-return [
+/*return [
     'app_id' => 'wwdfcb0c134ab7dc75',
 
     'app_secret' => 'RHyquXfMA2wo0vZZQAvp_4m0jgqDTj_FqaTedUH1sV8',
 
     'app_agent_id' => '1000003',
+
+    'wx_code_url' => 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=SCOPE&state=STATE#wechat_redirect',
+
+    'access_token_url' => "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=%s&corpsecret=%s",
+
+    'user_id_url' => "https://qyapi.weixin.qq.com/cgi-bin/user/getuserinfo?access_token=%s&code=%s",
+
+    'user_info_url' => "https://qyapi.weixin.qq.com/cgi-bin/user/get?access_token=%s&userid=%s",
+
+    'msg_url' => "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s",
+
+];*/
+
+return [
+    'app_id' => 'wwffcc8e626d73e98f',
+
+    'app_secret' => 'bSJSQAy_aWakifiNMl9i33OdPf51dungRj5lLYRuDrM',
+
+    'app_agent_id' => '1000048',
 
     'wx_code_url' => 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=SCOPE&state=STATE#wechat_redirect',
 
