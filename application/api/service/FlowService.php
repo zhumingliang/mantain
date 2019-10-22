61c0faefab8738a8cf22228baef61a5a5a421596
zhumingliang <353575156@qq.com>
2019-10-22 20:06:19 +0800

    修复-机服中心负责人/管理员不可见流程问题

--- application/api/service/FlowService.php
+++ application/api/service/FlowService.php
@@ -426,13 +426,16 @@ class FlowService
     //预定场地-发送信息给指定用户：李景文
     private function sendMsgForBookingPlace($wf_type, $wf_fid)
     {
+        echo $wf_fid;
+        echo $wf_type;
         $info = Db::table('maintain_' . $wf_type)->where('id', $wf_fid)->find();
+        print_r($info);
         if ($info) {
             $admin = AdminT::get($info->admin_id);
             $msg = "%s的%s于%s申请场地:%s。";
             $msg = sprintf($msg, $info->department, $admin->username, $info->create_time, $info->place);
             $msg .= "请机服中心相关人员进行跟进。";
-           // $user = AdminT::getUserIdWithUserName("李景文");
+            // $user = AdminT::getUserIdWithUserName("李景文");
             $user = AdminT::getUserIdWithUserName("黄锐芝");
             if (!empty($user['user_id'])) {
                 (new MsgService())->sendMsg($user['user_id'], $msg);
@@ -458,7 +461,7 @@ class FlowService
 
         //发送给张海滨
         $msg .= "请机服中心相关人员进行跟进。";
-       // $user = AdminT::getUserIdWithUserName("张海滨");
+        // $user = AdminT::getUserIdWithUserName("张海滨");
         $user = AdminT::getUserIdWithUserName("黄锐芝");
         if (!empty($user['user_id'])) {
             (new MsgService())->sendMsg($user['user_id'], $msg);
@@ -478,7 +481,7 @@ class FlowService
 
         //推送给凌丽珠
         $msg .= "请机服中心相关人员进行跟进。";
-       // $user = AdminT::getUserIdWithUserName("凌丽珠");
+        // $user = AdminT::getUserIdWithUserName("凌丽珠");
         $user = AdminT::getUserIdWithUserName("黄锐芝");
         if (!empty($user['user_id'])) {
             (new MsgService())->sendMsg($user['user_id'], $msg);
@@ -500,7 +503,7 @@ class FlowService
 
         //发送给李康
         $msg .= "请机服中心相关人员进行跟进。";
-       // $user = AdminT::getUserIdWithUserName("李康");
+        // $user = AdminT::getUserIdWithUserName("李康");
         $user = AdminT::getUserIdWithUserName("黄锐芝");
         if (!empty($user['user_id'])) {
             (new MsgService())->sendMsg($user['user_id'], $msg);
@@ -522,7 +525,7 @@ class FlowService
 
         //推送给张灵学
         $msg .= "请机服中心相关人员进行跟进。";
-       // $user = AdminT::getUserIdWithUserName("张灵学");
+        // $user = AdminT::getUserIdWithUserName("张灵学");
         $user = AdminT::getUserIdWithUserName("黄锐芝");
         if (!empty($user['user_id'])) {
             (new MsgService())->sendMsg($user['user_id'], $msg);
